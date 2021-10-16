68bbc5c9fe38d05f351c1c9d5ca95093d76aa293
Masha Zavalina <rennka@ya.ru>
2021-10-16 10:32:34 +0200

    validation email on the frontend

--- src/hooks/useFormWithValidation.js
+++ src/hooks/useFormWithValidation.js
@@ -1,11 +1,14 @@
 import { useState, useCallback } from 'react';
+import isEmail from 'validator/lib/isEmail';
 
+
+const nameReg = /^[a-zа-яё\s]+$/iu;
 
 export function useFormWithValidation() {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
-  const [isValidInputs, setIsValidInputs] = useState({});
+
 
   const handleChange = (event) => {
     const target = event.target;
@@ -13,19 +16,50 @@ export function useFormWithValidation() {
     const value = target.value;
     setValues({ ...values, [name]: value });
     setErrors({ ...errors, [name]: target.validationMessage });
-    setIsValid(target.closest('form').checkValidity());
-    setIsValidInputs({ ...isValidInputs, [name]: target.checkValidity() });
-  };
 
+    if (name === 'email') {
+        !isEmail(value) && !errors[name]
+            ? setErrors({
+                  ...errors,
+                  [name]: 'неправильный Email',
+              })
+            : setErrors({
+                  ...errors,
+                  [name]: target.validationMessage,
+              });
+
+        setIsValid(
+            target.closest('form').checkValidity() && isEmail(value)
+        );
+    } else if (name === 'name') {
+        !nameReg.test(value) && value.length > 0
+            ? setErrors({
+                  ...errors,
+                  [name]: 'разрешена кириллица, латиница, пробел, дефис',
+              })
+            : setErrors({
+                  ...errors,
+                  [name]: target.validationMessage,
+              });
+
+        setIsValid(
+            target.closest('form').checkValidity() &&
+                nameReg.test(value)
+        );
+    } else {
+        setIsValid(target.closest('form').checkValidity());
+    }
+};
+
+  
   const resetForm = useCallback(
-    (newValues = {}, newErrors = {}, newIsValid = false, newIsValidInputs = {}) => {
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
       setValues(newValues);
       setErrors(newErrors);
       setIsValid(newIsValid);
-      setIsValidInputs(newIsValidInputs);
     },
     [setValues, setErrors, setIsValid]
   );
 
-  return { values, handleChange, errors, isValid, resetForm, setValues ,isValidInputs };
+  return { values, handleChange, errors, isValid, resetForm, setValues  };
 }
